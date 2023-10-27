fa82165c14d9bd79df4908987ab21fa6f8a96cb8
pfwd <hello@websomatic.co.uk>
2023-10-27 11:59:06 +0100

    Tidying up config

--- wp-config.php
+++ wp-config.php
@@ -3,7 +3,14 @@ require_once (__DIR__.'/content/vendor/autoload.php');
 // Move the location of the content dir
 define('WP_CONTENT_DIR', dirname(__FILE__).'/content');
 
-$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+$s = '';
+if (!empty($_SERVER["HTTPS"])) {
+    if ($_SERVER["HTTPS"] === "on") {
+        $s = 's';
+    }
+}
+
+
 $sp = strtolower($_SERVER["SERVER_PROTOCOL"]);
 $protocol = substr($sp, 0, strpos($sp, "/")) . $s;
 $port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]);
